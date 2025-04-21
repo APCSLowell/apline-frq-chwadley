2d51f038f357c7ec341ee99f6e352ab0d67f3d3c
chwadley <chwadley@s.sfusd.edu>
2025-04-21 11:25:07 -0700

    first try

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,12 @@
-public class APLine
-{
-  /* your code here */
-  
+public class APLine {
+  private int a,b,c;
+  public APLine(int _a, int _b, int _c) {
+    a = _a; b = _b; c = _c;
+  }
+  public double getSlope() {
+    return -(double)a/b;
+  }
+  public boolean isOnLine(int x, int y) {
+    return (a*x + b*y + c)==0;
+  }
 }
